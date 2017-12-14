c7c3b48d7d2545d742f155a7072a1c1f1b7abc8e
leonidchetverikov <l.chetverikov@gmail.com>
2017-12-14 07:49:46 +0200

    1.0.2

--- UploadFile/UploadFile.go
+++ UploadFile/UploadFile.go
@@ -1,49 +1,67 @@
-package UploadFile
+package main
 
 import (
 	"os"
 	"net/http"
 	"bytes"
 	"fmt"
-	"log"
+	"mime/multipart"
+	"io"
+	"io/ioutil"
 )
 
-func newfileUploadRequest(path string, uri string) (*http.Request, error) {
-	file, err := os.Open(path)
+func newfileUploadRequest(filename string, targetUrl string) error {
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	// this step is very important
+	fileWriter, err := bodyWriter.CreateFormFile("file", filename)
 	if err != nil {
-		return nil, err
+		fmt.Println("error writing to buffer")
+		return err
 	}
-	defer file.Close()
 
-	body := &bytes.Buffer{}
+	// open file handle
+	fh, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening file")
+		return err
+	}
+	defer fh.Close()
 
-	req, err := http.NewRequest("POST", uri, body)
-	req.Header.Set("Content-Type", "application/octet-stream")
-	return req, err
+	//iocopy
+	_, err = io.Copy(fileWriter, fh)
+	if err != nil {
+		return err
+	}
+
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+
+	resp, err := http.Post(targetUrl, contentType, bodyBuf)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.Status)
+	fmt.Println(string(resp_body))
+	return nil
 }
 
 func main() {
 	args := os.Args[1:]
 	for i, _ := range args {
 		if i==0 && len(args)==2 {
-			request, err := newfileUploadRequest(args[i], args[i+1])
+			err := newfileUploadRequest(args[i], args[i+1])
 			if err != nil {
-				log.Fatal(err)
+				fmt.Println(err)
 			}
-			client := &http.Client{}
-			resp, err := client.Do(request)
-			if err != nil {
-				log.Fatal(err)
-			} else {
-				body := &bytes.Buffer{}
-				_, err := body.ReadFrom(resp.Body)
-				if err != nil {
-					log.Fatal(err)
-				}
-				resp.Body.Close()
-				fmt.Println(resp.StatusCode)
-				fmt.Println(resp.Header)
-			}
+
+
 		}
 
 	}
